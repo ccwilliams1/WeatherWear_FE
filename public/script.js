6b8f1de65bc0fdb41cdaa4d8aa04ce6ebee29177
Caleb <ccwilliams1@aggies.ncat.edu>
2023-02-25 02:53:13 -0500

    Added Cards

--- public/script.js
+++ public/script.js
@@ -42,41 +42,19 @@ function showError() {
 //WeatherAPI call
 const getWeatherData = (city) => {
   // Weather API endpoint URL
-  const weatherEndpoint = "https://api.weatherapi.com/v1/current.json";
+  const weatherEndpoint = "http://api.weatherapi.com/v1/forecast.json";
 
   // Weather API key
   const apiKey = "889bcb646a87439db43203647232102";
 
-  // Container in the html that holds the weather text boxes
-  const weatherContainer = document.querySelector("#weather-container");
-
   // Make a request to the WeatherAPI and display the results on the screen
-  fetch(`${weatherEndpoint}?key=${apiKey}&q=${city}`)
+  fetch(`${weatherEndpoint}?key=${apiKey}&q=${city}&days=2`)
     .then((response) => response.json())
     .then((data) => {
-      // Create an HTML string with the weather data
-
-      //If location is in the US, print City, State
-      country = data.location.country;
-      if (country === "USA" || country === "United States of America") {
-        document.getElementById(
-          "location"
-        ).innerHTML = `${data.location.name}, ${data.location.region}`;
-        //If location is not in US print City, Country
-      } else {
-        document.getElementById(
-          "location"
-        ).innerHTML = `${data.location.name}, ${data.location.country}`;
-      }
-
-      const weatherHTML = `
-              <p>Current temperature: ${data.current.temp_f} °F</p>
-              <p>Current condition: ${data.current.condition.text}</p>
-            `;
-
-      // Add the weather HTML to the weather container element
-      weatherContainer.innerHTML = weatherHTML;
+      console.log(data);
+      fillWeatherData(data);
       document.getElementById("btn-container").style.display = "block";
+      document.getElementById("cards-container").style.display = "block";
     })
     .catch((error) => {
       console.error(error);
@@ -101,4 +79,69 @@ const handleSearch = () => {
       document.querySelector("#weather-container").style.display = "block";
     }
   }, 3000);
+};
+
+const fillWeatherData = (data) => {
+  // Container in the html that holds the weather text boxes
+  const weatherContainer = document.querySelector("#weather-container");
+
+  //If location is in the US, print City, State
+  country = data.location.country;
+  if (country === "USA" || country === "United States of America") {
+    document.getElementById(
+      "location"
+    ).innerHTML = `${data.location.name}, ${data.location.region}`;
+    //If location is not in US print City, Country
+  } else {
+    document.getElementById(
+      "location"
+    ).innerHTML = `${data.location.name}, ${data.location.country}`;
+  }
+
+  const weatherHTML = `
+              <p>Current temperature: ${data.current.temp_f} °F</p>
+              <p>Current condition: ${data.current.condition.text}</p>
+            `;
+
+  // Add the weather HTML to the weather container element
+  weatherContainer.innerHTML = weatherHTML;
+
+  //Fill cards
+  // Get all the h5 elements on the page
+  const h4Elements = document.getElementsByTagName("h4");
+  const h5Elements = document.getElementsByTagName("h5");
+  let hour = new Date().getHours();
+  currDay = 0;
+
+  // Loop through each h5 element and log its content to the console
+  for (let i = 0; i < h4Elements.length; i++) {
+    let hourTemp =
+      data.forecast.forecastday[currDay].hour[hour].temp_f.toString();
+    let decIndex = hourTemp.indexOf(".");
+    decIndex == -1 ? (decIndex = hourTemp.length) : decIndex;
+
+    hour += 2;
+    if (hour >= 24) {
+      hour -= 24;
+    }
+
+    // Convert the hour to standard time
+    let meridian = "AM";
+    let standardHour = hour;
+    if (hour >= 12) {
+      meridian = "PM";
+      if (hour > 12) {
+        standardHour = hour - 12;
+      }
+    }
+    if (hour === 0) {
+      standardHour = 12;
+      currDay = 1;
+    }
+    let standardTime = `${standardHour}:00 ${meridian}`;
+    h5Elements[i].innerHTML = standardTime;
+    h4Elements[i].innerHTML = `${hourTemp
+      .toString()
+      .substring(0, decIndex)} °F`;
+  }
 };
