e3f84bee709b1e5f251cbce90dcac1c944471404
Caleb <ccwilliams1@aggies.ncat.edu>
2023-03-28 22:31:26 -0400

    Implemented edit and delete functions

--- public/tabs/myWardrobe/script.js
+++ public/tabs/myWardrobe/script.js
@@ -1,5 +1,6 @@
 /* MY WARDROBE SCRIPT */
 let addItemForm;
+let editItemForm;
 
 //When the Add Item to Closet Button is pressed
 const handleAdd = () => {
@@ -25,6 +26,50 @@ const handleAdd = () => {
   });
 };
 
+const handleEdit = (event, id) => {
+  console.log("Edit");
+  console.log(event);
+  editItemForm = document.querySelector("#edit-item-form");
+  editItemForm.style.display = "block";
+
+  // Handle form submit
+  document.querySelector("#cancel").addEventListener("click", () => {
+    editItemForm.display == "block"
+      ? (editItemForm.display = "none")
+      : (editItemForm.display = "block");
+  });
+  editItemForm.addEventListener("submit", (e) => {
+    // console.log(event.target["item-name"].value);
+    // console.log(event.target["item-type"].value);
+
+    e.preventDefault();
+    const itemName = e.target["item-name"].value;
+    const itemType = e.target["item-type"].value;
+
+    // Send form data to the database
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/edit-item");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({ itemName, itemType, id }));
+    document.getElementById("wardrobe-table").innerHTML = "";
+    handleView();
+  });
+};
+
+const handleDelete = (event, id) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/delete-item");
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(JSON.stringify({ id }));
+
+  handleView();
+
+  console.log("Delete");
+};
+
+//You're gonna make sure that the ID is transferred, then you're gonna pull it, then you're gonna pass it into the onclicks
+//Then when you make your SQL query you refer by ID instead of item_name
+
 //When view my closet is pressed
 const handleView = () => {
   document.getElementById("wardrobe-buttons").style.display = "none";
@@ -34,9 +79,12 @@ const handleView = () => {
     .then((data) => {
       // Build HTML table from response
       let tableHtml = "<table>";
-      tableHtml += "<tr><th>ID</th><th>Name</th><th>Type</th></tr>";
+      tableHtml += "<tr><th>Name</th><th>Type</th></tr>";
       data.forEach((item) => {
-        tableHtml += `<tr><td>${item.id}</td><td>${item.item_name}</td><td>${item.item_type}</td></tr>`;
+        itemType =
+          item.item_type[0].toUpperCase() +
+          item.item_type.substring(1, item.item_type.length);
+        tableHtml += `<tr><td>${item.item_name}</td><td>${itemType}</td><td><btn class="edit-btn" onclick="handleEdit(event,${item.id})">Edit</btn></td><td><btn class="delete-btn" onclick="handleDelete(event, ${item.id})">Delete</btn></td></tr>`;
       });
       tableHtml += "</table>";
 
