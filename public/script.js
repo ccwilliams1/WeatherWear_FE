cdffd7b2e20790814d2504ddddd0e5eddc9febba
Caleb <ccwilliams1@aggies.ncat.edu>
2023-04-15 21:05:25 -0400

    Added authentication support

--- public/script.js
+++ public/script.js
@@ -1,115 +1,56 @@
-//Replace paragraph elements with current weather data information
-let fillWeatherData = (data) => {
-  // Container in the html that holds the weather text boxes
-  const weatherContainer = document.querySelector("#weather-container");
+const signUpOverlay = document.getElementById("signUp");
+const signInOverlay = document.getElementById("signIn");
 
-  //If location is in the US, print City, State
-  country = data.location.country;
-  if (country === "USA" || country === "United States of America") {
-    document.getElementById(
-      "location"
-    ).innerHTML = `${data.location.name}, ${data.location.region}`;
+const container = document.getElementById("container");
 
-    //If location is not in US print City, Country
-  } else {
-    document.getElementById(
-      "location"
-    ).innerHTML = `${data.location.name}, ${data.location.country}`;
+signUpOverlay.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+signInOverlay.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
+
+function handleSignUp() {
+  const form = document.getElementById("signupForm");
+  const name = form.elements["name"].value;
+  const email = form.elements["email"].value;
+  const password = form.elements["password"].value;
+  // Send form data to the database
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/signup");
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(JSON.stringify({ name, email, password }));
+  //console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+}
+
+async function handleSignIn() {
+  const form = document.getElementById("signinForm");
+  const email = form.elements["email"].value;
+  const password = form.elements["password"].value;
+  try {
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if (!data.success) {
+      document.getElementById("errorText").innerHTML = data.message;
+    } else if (data.success) {
+      console.log(data.name);
+      console.log(data.email);
+
+      localStorage.setItem("name", data.name);
+      localStorage.setItem("email", email);
+      window.location.href = "home.html";
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
   }
-
-  const weatherHTML = `
-                  <p>Current temperature: ${data.current.temp_f} °F</p>
-                  <p>Current condition: ${data.current.condition.text}</p>
-                  <p>Chance of Rain: ${data.forecast.forecastday[0].day.daily_chance_of_rain}%</p>
-                `;
-
-  // Add the weather HTML to the weather container element
-  weatherContainer.innerHTML = weatherHTML;
-
-  //Fill cards
-  // Get all the h5 elements on the page
-  const h3Elements = document.getElementsByTagName("h3");
-  const h4Elements = document.getElementsByTagName("h4");
-  const h5Elements = document.getElementsByTagName("h5");
-  const cardContainer = document.getElementById("cards-container");
-  const imgElements = document.getElementsByTagName("img");
-  let hour = getHour(data);
-  currDay = 0;
-
-  // Loop through each h5 element and log its content to the console
-  for (let i = 0; i < h5Elements.length; i++) {
-    let hourTemp = Math.floor(
-      data.forecast.forecastday[currDay].hour[hour].temp_f
-    ).toString();
-
-    hour += 2;
-    if (hour >= 24) {
-      hour -= 24;
-    }
-
-    // Convert the hour to standard time
-    let meridian = "AM";
-    let standardHour = hour;
-    if (hour >= 12) {
-      meridian = "PM";
-      if (hour > 12) {
-        standardHour = hour - 12;
-      }
-    }
-    if (hour === 0) {
-      standardHour = 12;
-      currDay = 1;
-    }
-    let standardTime = `${standardHour}:00 ${meridian}`;
-    h5Elements[i].innerHTML = standardTime;
-    h3Elements[i].innerHTML =
-      data.forecast.forecastday[currDay].hour[hour].condition.text;
-    h4Elements[i].innerHTML = `${hourTemp} °F`;
-    imgElements[i].src =
-      data.forecast.forecastday[currDay].hour[hour].condition.icon;
-  }
-};
-
-//Retrieve current time data from api call and return exact hour
-let getHour = (data) => {
-  let dateString = data.current.last_updated;
-  const parts = dateString.split(" ");
-  const timeParts = parts[1].split(":");
-  const hour = parseInt(timeParts[0], 10);
-  return hour;
-};
-
-//If the user searches for a different location
-handleSearch = () => {
-  const locationSearch = document.getElementById("location-text");
-
-  //Pretend like you're actually searching for the location
-  document.getElementById("location").innerHTML = "Getting Your Location...";
-  //Hide the buttons to pretend to think
-  document.querySelector("#weather-container").style.display = "none";
-  document.getElementById("btn-container").style.display = "none";
-  document.getElementById("cards-container").style.display = "none";
-
-  //Wait 3 seconds to make it convincing
-  setTimeout(() => {
-    {
-      //Update the weather data with the new location
-      getWeatherData(locationSearch.value);
-      document.querySelector("#weather-container").style.display = "block";
-      document.getElementById("cards-container").style.display = "block";
-    }
-  }, 3000);
-};
-
-let weatherObject;
-const createWeatherObject = (data) => {
-  let day = data.forecast.forecastday[0].day;
-  weatherObject = {
-    high: day.maxtemp_f,
-    low: day.mintemp_f,
-    condition: day.condition.text,
-    chance_of_rain: day.daily_chance_of_rain,
-  };
-  console.log(weatherObject);
-  localStorage.setItem("weatherObject", JSON.stringify(weatherObject));
-};
+}
